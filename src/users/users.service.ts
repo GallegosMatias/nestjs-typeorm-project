d30ba2226b3ccc4a30374d277dfe2a30cfcf96ae
GallegosMatias <gallegosmatiasjoel@gmail.com>
2023-09-12 04:26:50 -0300

    Error Handling | HTTP Exception 404 at deleteUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -43,8 +43,14 @@ export class UsersService {
         return userFound;
     }
 
-    deleteUser(id: number) {
-        return this.userRepository.delete({ id });
+    async deleteUser(id: number) {
+        const result = await this.userRepository.delete({ id });
+
+        if(result.affected == 0) { 
+            return new HttpException('User not  found!', HttpStatus.NOT_FOUND);
+        }
+
+        return result;
     }
 
     updateUser(id: number, user: updateUserDto) {
