e65b06dfb0e0c41ec76cabe1ce80fbdb3bbd3f72
Gallegos <Matias.Gallegos@apexamerica.com>
2023-09-12 00:40:51 -0300

    Added CreateUser method

--- nestjs-mqsq-project/src/users/users.controller.ts
+++ nestjs-mqsq-project/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { createUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    createUser(@Body() newUser: createUserDto) {
+        return this.usersService.CreateUser(newUser)
+
+    }
+}
