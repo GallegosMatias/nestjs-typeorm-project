2aea09e4d704724a38f982589ecf855472afd745
Irungaray <lionelirungaray@gmail.com>
2023-09-22 21:42:18 -0300

    Extract env vars

---
+++ src/env.template.ts
@@ -0,0 +1,8 @@
+// Create a .env file
+// And set your own env vars
+export const DB_TYPE = '';
+export const DB_HOST = '';
+export const DB_PORT = 3306;
+export const DB_USERNAME = '';
+export const DB_PASSWORD = '';
+export const DB_NAME = '';
